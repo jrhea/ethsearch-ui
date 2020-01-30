a443ed0444485942efef029fafb41bfacdabb927
jonny rhea <jonathan.rhea@gmail.com>
2020-01-30 16:11:12 -0600

    added some styling to the nav and made a collapsing menu

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SearchInput from './js/components/container/SearchInput.jsx';
 import Result from './js/components/container/Result.jsx';
 import Query from '../solr-js/solr';
 import Gitcoin from '../img/gitcoin.svg'
+import Github from '../img/github.svg'
 
 const fields = ['cache','title','url','content','tstamp','_version_','anchor','digest','host','boost','id'];
 class App extends React.Component{
@@ -99,12 +100,29 @@ class App extends React.Component{
   render() {
     return (
       <div className="App">
-        <div className="form-row justify-content-center">
-          <a href="https://gitcoin.co/grants/240/ethsearch">
-            <img className='grants' src={Gitcoin}/>
-          </a>
-        </div>
-        <SearchInput value={this.state.urlParam} parentCallback = {this.updateURLParams}/>
+        <nav className="navbar navbar-expand-md navbar-dark bg-primary">
+            {/* <a href="/" className="navbar-brand">ethsearch</a> */}
+            <SearchInput value={this.state.urlParam} parentCallback = {this.updateURLParams}/>
+            <div className="navbar-collapse collapse flex-shrink-1 flex-grow-0 order-last" id="navbar7">
+              <ul className="navbar-nav">
+                  <li className="nav-item">
+                      <a className="nav-link " href="https://github.com/ethsearch" target="_blank">
+                        <img className='github' src={Github}/>
+                        <sub className='github-text text-white'>Github</sub>
+                      </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="https://gitcoin.co/grants/240/ethsearch" target="_blank">
+                        <img className='gitcoin' src={Gitcoin}/>
+                        <sub className='donate-text text-white'>Donate</sub>
+                    </a>
+                  </li>
+              </ul>
+            </div>
+            <button className="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbar7">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+        </nav>
         {
           this.state.results.map((item, index) => (
             <Result key={index} result={item} fields={fields}/>
